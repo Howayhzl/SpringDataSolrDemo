fb76ba3387b6dce8e794d1eb7ae72fbdc544606c
Howay <heweihzl@163.com>
2019-11-16 12:20:19 +0800

    条件查询完成

--- src/test/java/com/itcast/test/TestTemplate.java
+++ src/test/java/com/itcast/test/TestTemplate.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
 import org.springframework.data.solr.core.query.Query;
 import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.data.solr.core.query.result.ScoredPage;
@@ -57,8 +58,8 @@ public class TestTemplate {
             TbItem item = new TbItem();
             item.setId(i+1L);
             item.setTitle("华为METE10");
-            item.setCategory("手机");
-            item.setBrand("华为");
+            item.setCategory("手机"+i);
+            item.setBrand("华为"+i);
             item.setSeller("华为旗舰店");
             item.setGoodsId(i+10L);
             item.setPrice(new BigDecimal(3000.01));
@@ -74,6 +75,23 @@ public class TestTemplate {
         Query query = new SimpleQuery("*:*");
         query.setOffset(20);
         query.setRows(10);
+        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+        for (TbItem item : page.getContent()) {
+            System.out.println(item.getTitle()+" "+item.getPrice()+" "+item.getBrand());
+        }
+        System.out.println("总记录数："+page.getTotalElements());
+        System.out.println("总页数："+page.getTotalPages());
+    }
+
+
+    @Test
+    public void testPageQueryMultil(){
+        Query query = new SimpleQuery("*:*");
+        Criteria criteria = new Criteria("item_category").contains("手机");
+        criteria = criteria.and("item_brand").contains("2");
+        query.addCriteria(criteria);
+       /* query.setOffset(20); //开始索引
+        query.setRows(10);//每页记录数*/
         ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
         for (TbItem item : page.getContent()) {
             System.out.println(item.getTitle()+" "+item.getPrice()+" "+item.getBrand());
